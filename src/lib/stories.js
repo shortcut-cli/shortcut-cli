3df469fd738e4ec231ff1c780b1bc738453df0b1
Josh Beckman <email@andjosh.com>
2017-10-30 13:55:37 -0500

    fix:usr: Correctly filter out archived stories

--- src/lib/stories.js
+++ src/lib/stories.js
@@ -39,6 +39,9 @@ const filterStories = (program, projects) => { return (stories, index) => {
         });
         return story;
     }).filter(s => {
+        if (!program.archived && s.archived) {
+            return false;
+        }
         if (!(s.labels
             .map(l => `${l.id},${l.name}`).join(',') + '')
             .match(new RegExp(program.label, 'i'))) {
@@ -61,9 +64,6 @@ const filterStories = (program, projects) => { return (stories, index) => {
         if (!s.story_type.match(new RegExp(program.type, 'i'))) {
             return false;
         }
-        if (!program.archived && s.archived) {
-            return false;
-        }
         return true;
     });
     return filtered;
@@ -80,12 +80,12 @@ const printStory = (program) => { return (story) => {
         return `${o.profile.name} (` + chalk.bold(`${o.profile.mention_name}` + ')');
     });
     log(chalk.blue.bold(`#${story.id}`) + chalk.blue(` ${story.name}`));
-    log(`  Type:    ${story.story_type}/${story.estimate || '_'}`);
-    log(`  Label:   ${labels.join(', ')}`);
-    log('  Project: ' + chalk.bold(`#${story.project.id}`) + ` ${story.project.name}`);
-    log('  Owners:  ' + `${owners.join(', ') || '_'}`);
-    log('  State:   ' + chalk.bold(`#${story.workflow_state_id} `) + story.state.name);
-    log(`  URL:     https://app.clubhouse.io/story/${story.id}`);
+    log(`  Type:     ${story.story_type}/${story.estimate || '_'}`);
+    log(`  Label:    ${labels.join(', ')}`);
+    log('  Project:  ' + chalk.bold(`#${story.project.id}`) + ` ${story.project.name}`);
+    log('  Owners:   ' + `${owners.join(', ') || '_'}`);
+    log('  State:    ' + chalk.bold(`#${story.workflow_state_id} `) + story.state.name);
+    log(`  URL:      https://app.clubhouse.io/story/${story.id}`);
     if (story.archived) {
         log('  archived: ' + chalk.bold(story.archived));
     }
