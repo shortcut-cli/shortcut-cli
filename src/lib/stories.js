6ded944944b343f866f71d670775fa0d32814e81
Josh Beckman <email@andjosh.com>
2017-11-10 12:53:59 -0600

    new:usr: Allow find stories by created/updated date
    This also fixes a bug when searching by owner(s)

--- src/lib/stories.js
+++ src/lib/stories.js
@@ -28,6 +28,17 @@ const fetchStories = async (project) => {
 };
 const filterStories = (program, projects) => { return (stories, index) => {
     const project = projects[index];
+    let created_at = false;
+    if (program.created)
+        created_at = parseDateComparator(program.created);
+    let updated_at = false;
+    if (program.updated)
+        updated_at = parseDateComparator(program.updated);
+    let regexLabel = new RegExp(program.label, 'i');
+    let regexState = new RegExp(program.state, 'i');
+    let regexOwner = new RegExp(program.owner, 'i');
+    let regexText = new RegExp(program.text, 'i');
+    let regexType = new RegExp(program.type, 'i');
     const filtered = stories.map(story => {
         story.project = project;
         story.state = wf.states
@@ -44,24 +55,30 @@ const filterStories = (program, projects) => { return (stories, index) => {
         }
         if (!(s.labels
             .map(l => `${l.id},${l.name}`).join(',') + '')
-            .match(new RegExp(program.label, 'i'))) {
+            .match(regexLabel)) {
             return false;
         }
         if (!(s.workflow_state_id + ' ' + s.state.name)
-            .match(new RegExp(program.state, 'i'))) {
+            .match(regexState)) {
             return false;
         }
         if (program.owner) {
-            const ownerMatch = new RegExp(program.owner, 'i');
-            return s.owners.filter(o => {
+            const owned = s.owners.filter(o => {
                 return !!`${o.profile.name} ${o.profile.mention_name}`
-                    .match(ownerMatch);
+                    .match(regexOwner);
             }).length > 0;
+            if (!owned) return false;
         }
-        if (!s.name.match(new RegExp(program.text, 'i'))) {
+        if (!s.name.match(regexText)) {
             return false;
         }
-        if (!s.story_type.match(new RegExp(program.type, 'i'))) {
+        if (!s.story_type.match(regexType)) {
+            return false;
+        }
+        if (created_at && !created_at(s.created_at)) {
+            return false;
+        }
+        if (updated_at && !updated_at(s.updated_at)) {
             return false;
         }
         return true;
@@ -93,6 +110,24 @@ const printStory = (program) => { return (story) => {
     log();
     return story;
 };};
+
+const parseDateComparator = (arg) => {
+    const match = arg.match(/[0-9].*/) || { index: 0, '0': { length: 12 } };
+    const parsedDate = new Date(arg.slice(match.index));
+    const comparator = arg.slice(0, match.index);
+    return (date) => {
+        switch(comparator) {
+        case '<':
+            return new Date(date) < parsedDate;
+        case '>':
+            return new Date(date) > parsedDate;
+        case '=':
+        default:
+            return new Date(date.slice(0, match[0].length)).getTime()
+                == parsedDate.getTime();
+        }
+    };
+};
 
 
 module.exports = {
