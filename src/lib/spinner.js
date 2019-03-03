8f617b6657e3f3d93f908a0c110b794e00a6c1c8
Jean-Martin Archer <jm@jmartin.ca>
2019-03-03 09:51:39 -0800

    chg:dev: set spinner once

--- src/lib/spinner.js
+++ src/lib/spinner.js
@@ -1,9 +1,11 @@
 const Spinner = require('cli-spinner').Spinner;
 const spinner = (text) => {
-    return new Spinner({
+    const spin = new Spinner({
         text: text ? text : 'Loading... %s ',
         stream: process.stderr
     });
+    spin.setSpinnerString(27);
+    return spin
 };
 
 module.exports = spinner;
