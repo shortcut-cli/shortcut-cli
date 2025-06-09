850a783c327407d63bfefae7d9b1ea3358bb2a94
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2025-06-09 10:50:07 -0400

    Bump Prettier (#330)

--- src/lib/stories.ts
+++ src/lib/stories.ts
@@ -55,45 +55,38 @@ export interface StoryHydrated extends Story {
 }
 
 async function fetchEntities(): Promise<Entities> {
-    let [
-        projectsById,
-        statesById,
-        membersById,
-        groupsById,
-        epicsById,
-        iterationsById,
-        labels,
-    ] = await Promise.all([
-        client
-            .listProjects()
-            .then((r) => r.data)
-            .then(mapByItemId),
-        client
-            .listWorkflows()
-            .then((r) => r.data)
-            .then((wfs: Workflow[]) => wfs.reduce((states, wf) => states.concat(wf.states), []))
-            .then(mapByItemId),
-        client
-            .listMembers(null)
-            .then((r) => r.data)
-            .then(mapByItemStringId),
-        client
-            .listGroups()
-            .then((r) => r.data)
-            .then(mapByItemStringId),
-        client
-            .listEpics(null)
-            .then((r) => r.data)
-            .then(mapByItemId),
-        client
-            .listIterations(null)
-            .then((r) => r.data)
-            .then(mapByItemId),
-        client.listLabels(null).then((r) => r.data),
-    ]).catch((err) => {
-        log(`Error fetching workflows: ${err}`);
-        process.exit(2);
-    });
+    let [projectsById, statesById, membersById, groupsById, epicsById, iterationsById, labels] =
+        await Promise.all([
+            client
+                .listProjects()
+                .then((r) => r.data)
+                .then(mapByItemId),
+            client
+                .listWorkflows()
+                .then((r) => r.data)
+                .then((wfs: Workflow[]) => wfs.reduce((states, wf) => states.concat(wf.states), []))
+                .then(mapByItemId),
+            client
+                .listMembers(null)
+                .then((r) => r.data)
+                .then(mapByItemStringId),
+            client
+                .listGroups()
+                .then((r) => r.data)
+                .then(mapByItemStringId),
+            client
+                .listEpics(null)
+                .then((r) => r.data)
+                .then(mapByItemId),
+            client
+                .listIterations(null)
+                .then((r) => r.data)
+                .then(mapByItemId),
+            client.listLabels(null).then((r) => r.data),
+        ]).catch((err) => {
+            log(`Error fetching workflows: ${err}`);
+            process.exit(2);
+        });
 
     debug('response workflows, members, groups, projects, epics, iterations');
     return { projectsById, statesById, membersById, groupsById, epicsById, iterationsById, labels };
@@ -130,10 +123,8 @@ async function fetchStories(program: any, entities: Entities): Promise<Story[]> 
     );
 
     debug('request all stories for project(s)', projectIds.map((p) => p.name).join(', '));
-    return Promise.all(
-        projectIds.map((p) => client.listStories(p.id, null))
-    ).then((projectStories) =>
-        projectStories.reduce((acc, stories) => acc.concat(stories.data), [])
+    return Promise.all(projectIds.map((p) => client.listStories(p.id, null))).then(
+        (projectStories) => projectStories.reduce((acc, stories) => acc.concat(stories.data), [])
     );
 }
 
@@ -219,7 +210,7 @@ const findLabelNames = (entities: Entities, label: string) => {
     const labelMatch = new RegExp(label.split(',').join('|'), 'i');
     return entities.labels
         .filter((m) => !!`${m.id} ${m.name}`.match(labelMatch))
-        .map((m) => ({ name: m.name } as Label));
+        .map((m) => ({ name: m.name }) as Label);
 };
 
 const filterStories = (program: any, stories: Story[], entities: Entities) => {
