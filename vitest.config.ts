d1387f9a65e5138dd613683972ae7829ec81c4a2
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2026-03-10 11:06:17 -0400

    Add Codecov for test coverage (#384)

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
         coverage: {
             provider: 'v8',
             include: ['src/**/*.ts'],
+            reporter: ['text', 'lcov'],
         },
     },
 });
